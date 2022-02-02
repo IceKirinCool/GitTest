edf95959333e9db8d0a6a2209cc629f507553cc2
IceKirin <IceKirin@163.com>
2022-02-03 00:16:27 +0800

    1st Commit

---
+++ src/main/java/com/IceKirin/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.IceKirin.git;
+
+/**
+ * @author RLJ
+ * @date 2022-02-03 0:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git~");
+    }
+}
