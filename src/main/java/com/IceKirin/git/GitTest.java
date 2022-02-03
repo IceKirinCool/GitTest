a8cc0aff92fe6859ea6940b1bdd15dc8c7d207c8
IceKirin <IceKirin@163.com>
2022-02-03 20:55:47 +0800

    hot-fix 2st Commit

--- src/main/java/com/IceKirin/git/GitTest.java
+++ src/main/java/com/IceKirin/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello.git2~");
         System.out.println("hello.git3~");
         System.out.println("hello.git4~ Hot-fix");
+        System.out.println("hello.git5~ Hot-fix 2");
     }
 }
